1bd12b799a44e2e045bcbac62427a8144456be2d
atamakonkurii <morisudesu@gmail.com>
2022-02-06 13:09:11 +0900

    背景画像を設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center">
+    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center bg-cover bg-[url('/pw-bg.svg')]">
       <Head>
         <title>写真人狼</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <h1 className="text-5xl">
         Hello{" "}
-        <a className="text-blue-500 hover:text-blue-300 font-bold" href="https://nittai-one.com/zh-TW">
+        <a
+          className="text-blue-500 hover:text-blue-300 font-bold"
+          href="https://nittai-one.com/zh-TW"
+        >
           Nittai-one!
         </a>
       </h1>
